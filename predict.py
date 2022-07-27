0706086f47d11def1de097371aded36e8cec43f1
dinhchicong <dinhchicong@macs-MacBook-Air-2.local>
2022-07-27 22:11:05 +0700

    update predict

--- predict.py
+++ predict.py
@@ -1,43 +1,43 @@
 
 import csv
-from unittest import result
 import matplotlib.pyplot as plt
 
-def get_data_csv(path, time, type, choice):
+def get_data_csv(path):
 
     with open(path) as f:
         reader = csv.reader(f)
         count = -1
         for row in reader:
-            if row[0] == time:
-                count += 1
+            temperature = (int(row[1])  - 32) // 1.8
 
-            if count == type:
-                print(row)
-                return row[choice]
                 
 
             
-def find_data(root_path,date, time, type):
+def find_data(root_path):
     year = 2004
     sum = 0
     count = 0
     while year < 2022:
-        path = root_path + str(year) + '/' + str(year) + '-' + date + '.csv'
+        for month in range(1,13):
+            day = 0
+            while day<31:
+                day+=1
+                if month < 10:
+                    name_month = '0' + str(month)
+                else:
+                    name_month = str(month)
+                if day<10:
+                    name_day = '0' + str(day)
+                else:
+                    name_day = str(day) 
+                path = root_path + str(year) + '/' + str(year) + '-' +name_month +'-'+ name_day + '.csv'
+                try:
+                    get_data_csv(path)
+                    print(path)
+                except:
+                    break
         year+=1
-        data_tmp = get_data_csv(path, time, type, choice = 1)
-        if data_tmp is not None:
-            count += 1
-            sum += int(data_tmp)
-    result = sum/count 
-    result = (result - 32) // 1.8
-    return result
-    
 
 if __name__ == "__main__":
     root_path = './data/HCMC/'
-    date= '07-27'
-    time = '10:00'
-    type = 0
-    result = find_data(root_path,date,time,type)
-    print("Nhiệt độ ngày {} lúc {} là {} độ C".format('2022' + '-' + date, time, result))
+    result = find_data(root_path)
